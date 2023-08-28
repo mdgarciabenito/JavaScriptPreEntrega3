1e321cba946b64654702ac8bc67081af3909d8a3
Mariano García Benito <mdgarciabenito@gmail.com>
2023-08-28 15:39:34 -0300

    third commit

--- main.js
+++ main.js
@@ -1,72 +1,8 @@
-console.log ("*------------------------------------------------------------------------------*");
-
-console.log ("Constructor de Productos");
-
-class Producto {
-    constructor (nombre, tipo, cualidad, precio){
-        this.nombre = nombre;
-        this.tipo = tipo;
-        this.cualidad = cualidad;
-        this.precio = parseFloat (precio)
-    }
-}
-
-const tazaCeramicaA = new Producto ("Taza Cerámica A", "taza", "cerámica", 15);
-const tazaMagicaA = new Producto ("Taza Mágica A", "taza", "mágica", 25);
-const tazaPlasticaA = new Producto ("Taza Plástica A", "taza", "plástica", 15);
-const tazaCeramicaB = new Producto ("Taza Cerámica B", "taza", "cerámica", 15);
-const tazaMagicaB = new Producto ("Taza Mágica B", "taza", "mágica", 25);
-const tazaPlasticaB = new Producto ("Taza Plástica B", "taza", "plástica", 15);
-
-console.log ("*------------------------------------------------------------------------------*");
-
-Array
-
-console.log ("Array de Tazas");
-
-const tazas = [
-    new Producto ("Taza Cerámica A", "taza", "cerámica", 15),
-    new Producto ("Taza Mágica A", "taza", "mágica", 25),
-    new Producto ("Taza Plástica A", "taza", "plástica", 15),
-    new Producto ("Taza Cerámica B", "taza", "cerámica", 15),
-    new Producto ("Taza Mágica B", "taza", "mágica", 25),
-    new Producto ("Taza Plástica B", "taza", "plástica", 15)
-]
-
-console.log ("*------------------------------------------------------------------------------*");
-
-
-//Crear nuevo Array sumando IVA a los precios
-
-console.log ("Array de Tazas con IVA");
-
-const tazasIVA = tazas.map (item => (
-    {nombre: item.nombre, tipo: item.tipo, cualidad: item.cualidad, precio: item.precio*1.21}
-))
-console.log (tazasIVA)
-
-console.log ("*------------------------------------------------------------------------------*");
-
-console.log ("Cada Objeto del Array tazas IVA");
-
-/*tazas.forEach(function(taza){
-    console.log (taza)
-})*/
-
-tazasIVA.forEach ((taza) => console.log (taza));
-
-console.log ("*------------------------------------------------------------------------------*");
-
 let articulosCarrito = [];
-console.log (articulosCarrito);
 const listaProductos = document.getElementById("lista-productos");
-console.log (listaProductos);
 const carrito = document.getElementById("carrito");
-console.log (carrito);
 const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
-console.log (vaciarCarritoBtn);
-const contenedorCarrito = document.getElementById("lista-carrito tbody");
-console.log (contenedorCarrito);
+const contenedorCarrito = document.querySelector("#lista-carrito tbody"); 
 
 document.addEventListener('DOMContentLoaded', ()=>{
     if(JSON.parse(localStorage.getItem('carrito')) == null){
@@ -76,9 +12,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
         articulosCarrito = JSON.parse(localStorage.getItem('carrito'))
         console.log (articulosCarrito)
     }
+    carritoHTML();
 })
 
 listaProductos.addEventListener('click', agregarProducto);
+vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+carrito.addEventListener('click', eliminarProducto);
 
 function agregarProducto(evt){
     if(evt.target.classList.contains('agregar-carrito')){
@@ -87,78 +26,74 @@ function agregarProducto(evt){
     }
 }
 
+function eliminarProducto(evt){
+    console.log (evt.target.parentElement)
+    if(evt.target.classList.contains('borrar-producto')){
+        const producto = evt.target.parentElement.parentElement;
+        const productoId = producto.querySelector('a').getAttribute('data-id');
+        articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
+        carritoHTML();
+    }
+}
+
 function leerDatosProducto(item){
     const infoProducto = {
         imagen: item.querySelector('img').src,
         titulo: item.querySelector('h1').textContent,
         precio: item.querySelector('.precio').textContent,
+        id: item.querySelector('button').getAttribute('id'),
+        cantidad: 1,
     }
-    console.log (infoProducto)
-    articulosCarrito.push(infoProducto)
+    console.log(infoProducto)
+    if (articulosCarrito.some( item => item.id === infoProducto.id)){
+        const productos = articulosCarrito.map( producto => {
+            if (producto.id === infoProducto.id){
+                let cantidad = parseInt(producto.cantidad);
+                cantidad +=1;
+                producto.cantidad = cantidad;
+                return producto;
+            } else {
+                return producto
+            }
+        })
+        articulosCarrito = productos.slice();
+    } else { articulosCarrito.push(infoProducto)}
     carritoHTML()
 }
 
+
 function carritoHTML(){
+    limpiarCarrito();
     articulosCarrito.forEach(producto =>{
         const fila = document.createElement('tr');
         fila.innerHTML = `
-        <td><img src="${producto.imagen} width = "100"/></td>
+        <td><img src="${producto.imagen}" width = "200"/></td>
         <td>${producto.titulo}</td>
         <td>${producto.precio}</td>
+        <td>${producto.cantidad}</td>
         <td>
-            <a href="#" class="borrar-producto"> ❌ </a>
+            <a href="#" class="borrar-producto" data-id="${producto.id}"> ❌ </a>
         </td>
         `;
         contenedorCarrito.appendChild(fila)
     })
+    sincronizarStorage();
 }
 
-console.log ("*------------------------------------------------------------------------------*");
-//let res = document.getElementById("res")
-let clickBoton1 = document.getElementById("btnAgregarCarrito1");
-
-clickBoton1.addEventListener("click", respuestaClick1);
-
-function respuestaClick1 (){
-    console.log("Respuesta1");
+function sincronizarStorage(){
+    localStorage.setItem('articulosCarrito', JSON.stringify(articulosCarrito))
 }
 
-let clickBoton2 = document.getElementById("btnAgregarCarrito2");
-
-clickBoton2.addEventListener("click", respuestaClick2);
-
-function respuestaClick2 (){
-    console.log("Respuesta2");
+function limpiarCarrito(){
+    while (contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
 }
 
-let clickBoton3 = document.getElementById("btnAgregarCarrito3");
-
-clickBoton3.addEventListener("click", respuestaClick3);
-
-function respuestaClick3 (){
-    console.log("Respuesta3");
-}
-
-let clickBoton4 = document.getElementById("btnAgregarCarrito4");
-
-clickBoton4.addEventListener("click", respuestaClick4);
-
-function respuestaClick4 (){
-    console.log("Respuesta4");
-}
-
-let clickBoton5 = document.getElementById("btnAgregarCarrito5");
-
-clickBoton5.addEventListener("click", respuestaClick5);
-
-function respuestaClick5 (){
-    console.log("Respuesta5");
-}
-
-let clickBoton6 = document.getElementById("btnAgregarCarrito6");
-
-clickBoton6.addEventListener("click", respuestaClick6);
-
-function respuestaClick6 (){
-    console.log("Respuesta6");
+function vaciarCarrito(){
+    while (contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+        articulosCarrito = [];
+        sincronizarStorage();
+    }
 }
